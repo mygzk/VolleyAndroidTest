f6b59129456496b777e4035b0a931fff83be7877
guozhk <1565619659@qq.com>
2016-06-23 21:33:52 +0800

    test

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 
@@ -9,12 +10,20 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.demo.volley.MultipartRequest;
 import com.example.demo.volley.VolleyHelper;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     private String TAG = MainActivity.class.getSimpleName();
 
-    private String url = "http://172.16.0.10:8080/1.txt";
+    //  private String url = "http://172.16.0.10:8080/1.txt";
+    private String url = "http://192.168.253.13:80/test.php?id=12&s=2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         Volley.newRequestQueue(this.getApplicationContext());
 
         findViewById(R.id.btn_get).setOnClickListener(this);
+        findViewById(R.id.btn_post).setOnClickListener(this);
+        findViewById(R.id.btn_post2).setOnClickListener(this);
 
     }
 
@@ -44,12 +55,107 @@ public class MainActivity extends Activity implements View.OnClickListener {
         VolleyHelper.startRequest(stringRequest);
     }
 
+    String rootPath = Environment.getExternalStorageDirectory().getPath();
+    String f0 = rootPath + File.separator + "police/1.jpg";
+    String f1 = rootPath + File.separator + "sdcard/3.jpg";
+    String f2 = rootPath + File.separator + "4.jpg";
+    String f3 = rootPath + File.separator + "5.jpg";
+
+    private void postTest() {
+        File file0 = new File(f0);
+        File file1 = new File(f1);
+        File file2 = new File(f2);
+        File file3 = new File(f3);
+        if (file0.exists()) {
+            Log.e("file", "file0 is exists");
+        }
+        if (file1.exists()) {
+            Log.e("file", "file1 is exists");
+        }
+        if (file2.exists()) {
+            Log.e("file", "file2 is exists");
+        }
+        if (file3.exists()) {
+            Log.e("file", "file3 is exists");
+        }
+        Map<String, String> parms = new HashMap<String, String>();
+        parms.put("s", "ss");
+        parms.put("s1", "ss1");
+        MultipartRequest multipartRequest = new MultipartRequest(url,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                },
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                },
+                "file",
+                file0,
+                parms);
+
+    }
+
+    private void postTest1() {
+        File file0 = new File(f0);
+        File file1 = new File(f1);
+        File file2 = new File(f2);
+        File file3 = new File(f3);
+        if (file0.exists()) {
+            Log.e("file", "file0 is exists");
+        }
+        if (file1.exists()) {
+            Log.e("file", "file1 is exists");
+        }
+        if (file2.exists()) {
+            Log.e("file", "file2 is exists");
+        }
+        if (file3.exists()) {
+            Log.e("file", "file3 is exists");
+        }
+        Map<String, String> parms = new HashMap<String, String>();
+        parms.put("s", "ss");
+        parms.put("s1", "ss1");
+
+        List<File> list = new ArrayList<File>();
+        list.add(new File(f0));
+        list.add(new File(f2));
+        list.add(new File(f3));
+        MultipartRequest multipartRequest = new MultipartRequest(url,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                },
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                },
+                "file1",
+                list,
+                parms);
+
+    }
+
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_get:
                 getTest(url);
+                break;
+            case R.id.btn_post:
+                postTest();
+                break;
+            case R.id.btn_post2:
+                postTest1();
                 break;
         }
     }
