29620ceeaeca4f19d871ec0dd1e41b4c175e80ab
guozhk <1565619659@qq.com>
2016-07-04 15:12:12 +0800

    del

--- app/src/main/java/com/example/demo/App.java
+++ app/src/main/java/com/example/demo/App.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import android.app.Application;
 
-import com.example.demo.volley.VolleyHelper;
-
 /**
  * Created by guozhk on 16-6-2.
  */
@@ -12,6 +10,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        VolleyHelper.initialization(this);
+
     }
 }
